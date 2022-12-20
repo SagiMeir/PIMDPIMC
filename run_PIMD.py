75062f579fd5551854ec7c078c6e5bb0209f5970
Sagi Meir <sagim00@gmail.com>
2022-12-20 14:21:18 +0200

    fixed sum issues with stagging trans

--- run_PIMD.py
+++ run_PIMD.py
@@ -21,19 +21,20 @@ omega=np.sqrt(K_harm/mass)
 parms={"omega": omega}
 T = hbar * omega / BOLTZMANN / 6 # temperature
 
-# one atomic unit time in hartree units is 2.4188843265857(47)×10−17 sec
-# mysim = Simulation( dt=100/2.4188843265857, L=11.3E+2 , temp=T, Nsteps=1000000, 
-#                        mass=1, R=np.zeros( (1,3) ),beads=64, ftype="Harm", 
-#                        kind=["Ar"], printfreq=100, fac=1,thermo_type="Langevin")
+#one atomic unit time in hartree units is 2.4188843265857(47)×10−17 sec
+
+mysim = Simulation( dt=100/2.4188843265857, L=11.3E+2 , temp=T, Nsteps=10000, 
+                        mass=1, R=np.zeros( (1,3) ),beads=4, ftype="Harm", 
+                        kind=["Ar"], printfreq=100, fac=1)
 
 # mysim = Simulation( dt=100/2.4188843265857, L=11.3E+2 , temp=T, Nsteps=100000, 
 #                        mass=1, R=np.zeros( (1,3) ),beads=128, ftype="Harm", 
 #                        kind=["Ar"], printfreq=100, fac=1,thermo_type="Langevin",outname="128sim.log")
 
-T = hbar * omega / BOLTZMANN / 6
-mysim = Simulation( dt=100/2.4188843265857, L=11.3E+2 , temp=T, Nsteps=100000, 
-                        mass=1, R=np.zeros( (1,3) ),beads=32, ftype="Harm", 
-                        kind=["Ar"], printfreq=100, fac=1,thermo_type="Langevin",outname="6hwb_sim.log")
+# T = hbar * omega / BOLTZMANN / 6
+# mysim = Simulation( dt=100/2.4188843265857, L=11.3E+2 , temp=T, Nsteps=100000, 
+#                         mass=1, R=np.zeros( (1,3) ),beads=32, ftype="Harm", 
+#                         kind=["Ar"], printfreq=100, fac=1,thermo_type="Langevin",outname="6hwb_sim.log")
 
 
 mysim.run(**parms)
